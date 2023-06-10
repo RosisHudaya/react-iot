8530ff86a61a8f00408d2dce7bfe09cd8c5aa210
RosisHudaya <rosishudayaputra02@gmail.com>
2023-06-10 19:47:06 +0700

    instalasi laravel-react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import React from "react";
+import ReactDOM from "react-dom";
+import ExampleComponent from "./components/ExampleComponent";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ExampleComponent />
+    </React.StrictMode>,
+    document.getElementById("app")
+);
---
+++ resources/js/components/ExampleComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ExampleComponent() {
+    return (
+        <div>
+            <h1>Contoh Komponen ReactJ test </h1>
+        </div>
+    );
+}
+
+export default ExampleComponent;
